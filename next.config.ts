0290e0c1effd0bdde0e0e440f229cd61318d63ad
SernaAndrew <andrew.sernaq@gmail.com>
2024-11-20 15:26:42 -0500

    agregacion de links de busqueda de imagenes

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,16 @@
-import { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['www.example.com'], // Reemplaza con el dominio de las imágenes externas que usas
+    domains: [
+      'upload.wikimedia.org', // Wikimedia
+      'm.media-amazon.com',   // Amazon media
+      'i.imgur.com',          // Imgur
+      'image.tmdb.org',       // TMDB (The Movie Database)
+      'e.rpp-noticias.io',
+      'occ-0-8407-114.1.nflxso.net'
+      // Agrega aquí otros dominios que planeas usar
+    ],
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
